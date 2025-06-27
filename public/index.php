909b7939238dca87b67ac1d5472a9a1482e72d03
ManuelDAssoro <ManuelDAssoro@users.noreply.github.com>
2025-06-27 15:11:51 -0300

    add Bootstrap CSS and JS links to index.php

--- public/index.php
+++ public/index.php
@@ -6,8 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entornos</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-    
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
