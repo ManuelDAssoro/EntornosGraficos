072b441cb7a4d8e345c3cf8040a0b002f7643758
ManuelDAssoro <ManuelDAssoro@users.noreply.github.com>
2025-06-27 18:12:33 -0300

    add basic home page

--- public/index.php
+++ public/index.php
@@ -9,6 +9,24 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">🛍️ Mi Shopping</a>
+      <div class="d-flex">
+        <!-- Falta hacer la pagina de login.php -->
+        <a href="login.php" class="btn btn-outline-light me-2">Login</a>
+        <a href="register.php" class="btn btn-light">Registro</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container text-center mt-5">
+    <h1>Bienvenido a Mi Shopping</h1>
+    <p class="lead">Encuentra los mejores productos al mejor precio.</p>
+    <a href="register.php" class="btn btn-primary btn-lg mt-3">Comenzar ahora</a>
+  </div>
+
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
