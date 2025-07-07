9f34f20c8c882edd99a8d76a7978a43cf0fe0c70
ManuelDAssoro <ManuelDAssoro@users.noreply.github.com>
2025-07-06 22:25:35 -0300

    fix path in ver_usuarios.php

--- public/test/ver_usuarios.php
+++ public/test/ver_usuarios.php
@@ -1,6 +1,6 @@
 <?php
 // este codigo es para poder ver los usuarios de la base de datos
-require_once '../config/db.php';
+require_once '../../config/db.php';
 $stmt = $pdo->query("SELECT codUsuario, nombreUsuario, tipoUsuario, estado FROM usuarios");
 echo "<pre>";
 foreach ($stmt as $row) {
