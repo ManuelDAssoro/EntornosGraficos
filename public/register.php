9d4d8927df620f47b761d08bc1189e25c78d65a8
FrancoP <francorodriguez429@gmail.com>
2025-07-28 21:23:14 -0300

    boostrap icons en el register

--- public/register.php
+++ public/register.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrarse</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body>
     <div class="container mt-5">
